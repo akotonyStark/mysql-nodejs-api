606f8c274581220c47300342bdf99c890954ad43
Augustine Akoto Larbi-Ampofo <augustine.larbi-ampofo@persol.net>
2021-11-09 15:51:09 +0000

    middleware added

--- server/db/index.js
+++ server/db/index.js
@@ -13,6 +13,15 @@ const pool = mysql.createPool({
 //create a custom db object for making queries
 let homerDB = {};
 
+pool.getConnection((error, conn) => {
+    if(error){
+        console.log(error)
+    }
+    else{
+        console.log('Conncected as id: ', conn.threadId)
+    }
+})
+
 //getting all users
 homerDB.getAllUsers = () => {
     return new Promise((resolve, reject) => {
@@ -30,6 +39,47 @@ homerDB.getAllUsers = () => {
 homerDB.getOneUser = (id) => {
     return new Promise((resolve, reject) => {
         pool.query(`SELECT * from user_login_account WHERE id = ?`, [id],  (error, results) => {
+            if(error){
+                return reject(error)
+            }
+            return resolve(results)
+        }); 
+    });
+}
+
+homerDB.addUser = (user) => {
+   
+    return new Promise((resolve, reject) => {
+        pool.query(`INSERT INTO user_login_account VALUES ('${user.id}', '${user.username}', '${user.email}', '${user.password}')`,  (error, results) => {
+            if(error){
+                return reject(error)
+            }
+            return resolve(results)
+        }); 
+    });
+}
+
+
+homerDB.updateUser = (user) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`UPDATE user_login_account 
+                    SET email = '${user.email}',
+                    username = '${user.username}',
+                    password = '${user.password}'
+                    WHERE id = ?`, user.id,  (error, results) => {
+            if(error){
+                return reject(error.sqlMessage)
+            }
+            return resolve(results)
+        }); 
+    });
+}
+
+
+//deleting one user
+homerDB.deleteOneUser = (id) => {
+    return new Promise((resolve, reject) => {
+        pool.query(`DELETE FROM user_login_account WHERE id = ?`, [id],  (error, results) => {
             if(error){
                 return reject(error)
             }
--- server/routes/index.js
+++ server/routes/index.js
@@ -19,13 +19,13 @@ router.get('/getUsers', async (req, res, next) => {
 });
 
 
-//getone user
+//get one user
 router.get('/getUser/:id', async (req, res, next) => {
-    //res.json({test: 'test'})
+    
     try{
         let results = await db.getOneUser(req.params.id);
         if(results.length === 0){
-            res.status(404).send()
+            res.status(404).send({Message: 'User not found'})
         }
         res.json(results)
     }catch(e){
@@ -33,5 +33,63 @@ router.get('/getUser/:id', async (req, res, next) => {
         res.sendStatus(500)
     }
 });
+
+
+//add one user
+router.post('/addUser', async (req, res, next) => {
+  
+    try{
+        await db.addUser(req.body);
+        res.status(201).send(req.body)
+    }catch(e){
+        console.log(e)
+        res.status(400).send(e)
+    }
+});
+
+
+//update userinfor
+router.patch('/updateUserInfo', async (req, res) => {
+   
+        const keys = Object.keys(req.body)
+        const allowedPropertyUpdates = ['id', 'username', 'email', 'password', 'token']
+
+        const isValidOperation = keys.every((key) => {
+            return allowedPropertyUpdates.includes(key)
+        })
+
+       if(!isValidOperation){
+           return res.status(400).send({error: 'Invalid data provided'})
+       }
+
+        try{
+            
+            //validation can be put on the request body
+            console.log(req.body)
+            await db.updateUser(req.body);
+            res.status(200).send({Message: 'Record updated successfully'});
+        
+        }catch(e){
+            console.log(e)
+            res.status(400).send(e)
+        }
+})
+
+//delete user
+router.delete('/removeUserById/:id', async (req, res, next) => {
+    
+    try{
+        let apiResponseBody = await db.deleteOneUser(req.params.id);
+        if(apiResponseBody.affectedRows === 0){
+           return res.status(404).send({Message: 'User not found'})
+           //return res.status(204).send() ---technically this should be the right now
+        }        
+        res.status(200).send({Message: 'User has successfully been removed'})
+    }catch(e){
+        console.log(e)
+        res.sendStatus(500)
+    }
+});
+
 
 module.exports = router
