12b6a21a1c5a837d284de897d40981f66802c006
Augustine Akoto Larbi-Ampofo <alarbiampofo@gmail.com>
2021-11-16 07:47:34 +0000

    Update auth.js

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,26 +1,44 @@
 const jwt = require('jsonwebtoken')
-const db = require('../db')
+const dbUserContext = require('../db/user')
 
 const auth = async(req, res, next) => {
+
     try{
-        const token = req.header('Authorization').replace('Bearer', '')
-        console.log("Token: " , token);
-        const decoded = jwt.verify(token, process.env.JWT_SECRET || 'homer_secret')
+        const user_payload = req.body   
+            
+        //FIND USER BY CREDENTIALS
+        const response = await dbUserContext.findByCredentials(user_payload)
+        const user =  response[0]
+        //console.log('User:', user)
+        const header = req.headers['authorization']
+        console.log("header", header)
 
-        let user = await db.getOneUser(req.par);
         if(!user){
-            return res.status(401).send({Message: 'Unauthorized access'})
+            return res.status(404).send({Error: 'Invalid user credentials'})
         }
+        
+        else if(typeof header != 'undefined'){
+            const bearer = header.split(' ')
+            const token = bearer[1]
+            //console.log("Token: ", token)
+            user.token = token
 
-        console.log(token)
-        req.token = token
-        req.user = user
-        next()
-
+            console.log('Auth User:', user)
+            await dbUserContext.login(user).then(results => {
+                return res.send(results)
+            }).catch(error => {
+                return res.status(401).send({"Error:": error})
+            })
+            //res.send(user)
+            next()
+        }
+        else{
+            res.sendStatus(403)
+        }
     }catch(e){
-        res.status(401).send({Message: 'Unauthorized access'})
+        console.log(e)
     }
-} 
+}
 
 
 module.exports = auth
